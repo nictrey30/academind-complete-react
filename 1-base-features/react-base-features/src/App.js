8ec845352c3bf5526cc280d5dd2e192f2364f788
Traian <niculescu.traian.ct@gmail.com>
2020-12-01 02:32:54 +0200

    1-base-features

--- 1-base-features/react-base-features/src/App.js
+++ 1-base-features/react-base-features/src/App.js
@@ -1,20 +1,105 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './components/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Max',
+        age: 28
+      },
+      {
+        name: 'Manu',
+        age: 29
+      },
+      {
+        name: 'Stephanie',
+        age: 26
+      }
+    ],
+    otherState: 'some other value'
+  };
+
+  handleSwitchNames = (newName) => {
+    this.setState({
+      persons: [
+        {
+          name: newName,
+          age: 28
+        },
+        {
+          name: 'Manu',
+          age: 29
+        },
+        {
+          name: 'Stephanie',
+          age: 26
+        }
+      ]
+    });
+  };
+
+  // change the name of only the second person
+  handleNameChanged = (e) => {
+    this.setState({
+      persons: this.state.persons.map((person, index) =>
+        index === 1
+          ? {
+              name: e.target.value,
+              age: person.age
+            }
+          : person
+      )
+    });
+  };
+
   render() {
+    const buttonStyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid grey',
+      outline: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer'
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <h1>Hi, I'm a React App</h1>
+        <p>A second paragraph</p>
+        {/* <button onClick={this.handleSwitchNames.bind(this, 'Ion')}> */}
+        <button
+          style={buttonStyle}
+          onClick={() => this.handleSwitchNames('Ion')}
+        >
+          Switch Name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          handleSwitchNames={this.handleSwitchNames}
+        >
+          My hobbies: racing
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          handleSwitchNames={this.handleSwitchNames}
+          handleNameChanged={this.handleNameChanged}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          handleSwitchNames={this.handleSwitchNames}
+        />
       </div>
     );
+    // 1st - the element we create, 2nd - configuration, 3rd...etc. the children nested inside the element
+    //   return React.createElement(
+    //     'div',
+    //     { className: 'App' },
+    //     React.createElement('h1', null, 'This is working now')
+    //   );
   }
 }
 
---
+++ 1-base-features/react-base-features/src/components/Person.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../css/Person.css';
+
+const Person = (props) => {
+  return (
+    <div className='Person'>
+      <p
+        // pass method to the state that change the state of the parent component
+        onClick={() => {
+          props.handleSwitchNames('Ana-Maria');
+        }}
+      >
+        I'm {props.name} and I am {props.age} years old.
+      </p>
+      <p>props.children</p>
+      {/* the event will be passed automaticall by React to the handleNameChanged function */}
+      <input
+        type='text'
+        defaultValue={props.name}
+        onChange={props.handleNameChanged}
+      />
+    </div>
+  );
+};
+
+export default Person;
