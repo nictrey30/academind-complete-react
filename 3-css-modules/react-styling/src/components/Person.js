d5a9f65e0949e4703ac1023f0a3f03796a9de799
Traian <niculescu.traian.ct@gmail.com>
2020-12-07 01:42:25 +0200

    finished CSS Modules

--- 3-css-modules/react-styling/src/components/Person.js
+++ 3-css-modules/react-styling/src/components/Person.js
@@ -1,25 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-// import '../css/Person.css';
-
-// every method provided by this 'styled' object returns a React component
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  margin-bottom: 20px;
-  text-align: center;
-
-  p:hover {
-    text-decoration: line-through;
-  }
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
+import classes from '../css/Person.module.css';
 
 const Person = ({
   person,
@@ -28,19 +8,17 @@ const Person = ({
   children
 }) => {
   return (
-    // <div className='Person' style={style}>
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={() => handleDeletePerson(person.id)}>
         I'm {person.name} and I am {person.age} years old.
       </p>
       <p>{children}</p>
-      {/* the event will be passed automaticall by React to the handleNameChanged function */}
       <input
         type='text'
         defaultValue={person.name}
         onChange={(e) => handleNameChanged(e, person.id)}
       />
-    </StyledDiv>
+    </div>
   );
 };
 
