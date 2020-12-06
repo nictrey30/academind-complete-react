6bbdc1b8001f76abcd4e0f0e4d046a8647a6a661
Traian <niculescu.traian.ct@gmail.com>
2020-12-06 12:28:09 +0200

    setting class names dinamically

--- 2-list-conditionals/react-list-condtionals/src/App.js
+++ 2-list-conditionals/react-list-condtionals/src/App.js
@@ -53,16 +53,19 @@ class App extends Component {
   render() {
     // console.log(this.state.togglePersons);
     const buttonStyle = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid grey',
       outline: 'none',
       borderRadius: '5px',
+      padding: '5px',
       cursor: 'pointer'
     };
 
     let persons = null;
 
+    // if this condition is true => persons can be seen
     if (this.state.togglePersons) {
       persons = (
         <div>
@@ -78,43 +81,20 @@ class App extends Component {
           })}
         </div>
       );
+      buttonStyle.backgroundColor = 'red';
     }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) classes.push('red');
+    if (this.state.persons.length <= 1) classes.push('bold');
 
     return (
       <div className='App'>
-        <ol>
-          <li>
-            Create an input field (in App component) with a change listener
-            which outputs the length of the entered text below it (e.g. in a
-            paragraph).
-          </li>
-          <li>
-            Create a new component (- ValidationComponent) which receives the
-            text length as a prop
-          </li>
-          <li>
-            Inside the ValidationComponent, either output "Text too short" or
-            "Text long enough" depending on the text length (e.g. take 5 as a
-            minimum length)
-          </li>
-          <li>
-            Create another component (- CharComponent) and style it as an inline
-            box (- display: inline-block, padding: 16px, text-align: center,
-            margin: 16px, border: 1px solid black).
-          </li>
-          <li>
-            Render a list of CharComponents where each CharComponent receives a
-            different letter of the entered text (in the initial input field) as
-            a prop.
-          </li>
-          <li>
-            When you click a CharComponent, it should be removed from the
-            entered text.
-          </li>
-          <div>
-            <ParagraphLength />
-          </div>
-        </ol>
+        {/* setting the class of this p tag dinamically based upon the length of the persons */}
+        <p className={classes.join(' ')}>This is really working</p>
+        <div>
+          <ParagraphLength />
+        </div>
         {/* <button onClick={this.handleSwitchNames.bind(this, 'Ion')}> */}
         <button style={buttonStyle} onClick={this.handleTogglePersons}>
           Toggle Persons
