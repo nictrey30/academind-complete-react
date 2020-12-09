9752e0e77297b383a9ab7d0065cf33c56d6067a7
Traian <niculescu.traian.ct@gmail.com>
2020-12-09 19:58:35 +0200

    moving out presentational logic from App.js

--- 4-diving-deeper/src/components/Cockpit/Cockpit.js
+++ 4-diving-deeper/src/components/Cockpit/Cockpit.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import classes from '../../containers/App.module.css';
+import classes from './Cockpit.module.css';
 import ParagraphLength from '../ParagraphLength';
 
-const Cockpit = ({ handleTogglePersons, persons, btnClass }) => {
+const Cockpit = ({ handleTogglePersons, persons, showPersons }) => {
   const assignedClasses = [];
-  if (persons.length <= 2) assignedClasses.push(classes.red); // "App_red__ai-vW"
-  if (persons.length <= 1) assignedClasses.push(classes.bold); // "App_bold__Gusdv"
+  if (persons.length <= 2) assignedClasses.push(classes.red); // "Cockpit_red__ai-vW"
+  if (persons.length <= 1) assignedClasses.push(classes.bold); // "Cockpit_bold__Gusdv"
+
+  console.log(showPersons);
+  let btnClass = '';
+
+  if (showPersons) {
+    btnClass = classes.Red;
+  }
+
   return (
-    <>
+    <div className={classes.Cockpit}>
       <h1>HI, I'm a React App</h1>
       {/* setting the class of this p tag dinamically based upon the length of the persons */}
       <p className={assignedClasses.join(' ')}>This is really working</p>
@@ -17,7 +25,7 @@ const Cockpit = ({ handleTogglePersons, persons, btnClass }) => {
       <button className={btnClass} onClick={handleTogglePersons}>
         Toggle Persons
       </button>
-    </>
+    </div>
   );
 };
 export default Cockpit;
