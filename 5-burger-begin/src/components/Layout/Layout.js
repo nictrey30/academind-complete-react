4f336f2a3c6886e9e63426e5b5485d9bf3c468e9
Traian <traian30y@yahoo.co.uk>
2020-12-20 02:42:17 +0200

    started implementation of the BUrgerBuilder Container

--- 5-burger-begin/src/components/Layout/Layout.js
+++ 5-burger-begin/src/components/Layout/Layout.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 
 const layout = (props) => {
   return (
-    <>
+    <Aux>
       <div>Toolbar, SideDrawer, Backdrop</div>
       {/* output the component we wrap with this layout */}
-      <main>{props.children}</main>
-    </>
+      <main className={classes.Content}>{props.children}</main>
+    </Aux>
   );
 };
+
+export default layout;
