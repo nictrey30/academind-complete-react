00035782bf63b3b36078cfb0af90af917c65f7e2
Traian <traian30y@yahoo.co.uk>
2020-12-31 13:13:42 +0200

    added logo to the toolbar

--- 5-burger-begin/src/components/Layout/Layout.js
+++ 5-burger-begin/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 
 const layout = (props) => {
   return (
     <Aux>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
+      <div>SideDrawer, Backdrop</div>
       {/* output the component we wrap with this layout */}
       <main className={classes.Content}>{props.children}</main>
     </Aux>
