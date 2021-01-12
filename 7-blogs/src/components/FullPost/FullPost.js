c33694c84e49052b15da1ef0f776e6f929dcf919
Traian <niculescu.traian.ct@gmail.com>
2021-01-12 22:22:19 +0200

    load post in FullPost with componentDidUpdate

--- 7-blogs/src/components/FullPost/FullPost.js
+++ 7-blogs/src/components/FullPost/FullPost.js
@@ -8,9 +8,12 @@ class FullPost extends Component {
   };
   async componentDidUpdate() {
     // Use this as an opportunity to operate on the DOM when the component has been updated. This is also a good place to do network requests as long as you compare the current props to previous props (e.g. a network request may not be necessary if the props have not changed).
+    // BEWARE OF CREATING INFINITE LOOPS!!!
     if (this.props.id) {
       if (
+        // if there is no post loaded
         !this.state.loadedPost ||
+        // if we have a loaded post, but we clicked on another post
         (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
       )
         try {
@@ -35,7 +38,7 @@ class FullPost extends Component {
       post = (
         <div className='FullPost'>
           <h1>{this.state.loadedPost.title}</h1>
-          <p>{this.state.loadedPost.content}</p>
+          <p>{this.state.loadedPost.body}</p>
           <div className='Edit'>
             <button className='Delete'>Delete</button>
           </div>
