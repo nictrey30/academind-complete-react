b8987fde39419db52dd3146825b5405764e256b6
Traian <niculescu.traian.ct@gmail.com>
2021-01-12 17:10:13 +0200

    getiing posts with axios and render them

--- 7-blogs/src/components/Post/Post.js
+++ 7-blogs/src/components/Post/Post.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
-        </div>
-    </article>
+const post = ({ title, author }) => (
+  <article className='Post'>
+    <h1>{title}</h1>
+    <div className='Info'>
+      <div className='Author'>{author}</div>
+    </div>
+  </article>
 );
 
 export default post;
--- 7-blogs/src/containers/Blog/Blog.js
+++ 7-blogs/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,38 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  state = {
+    posts: []
+  };
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => ({ ...post, author: 'Max' }));
+      this.setState({ posts: updatedPosts });
+    } catch (error) {
+      console.log(error.message);
     }
+  }
+  render() {
+    // array of jsx elements
+    const posts = this.state.posts.map((post) => (
+      <Post key={post.id} title={post.title} author={post.author} />
+    ));
+    return (
+      <div>
+        <section className='Posts'>{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
