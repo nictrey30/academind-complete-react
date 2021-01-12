5190abf2a6dfa07881095c2398a1164c08dd40bd
Traian <niculescu.traian.ct@gmail.com>
2021-01-13 01:35:31 +0200

    adding axios interceptors to index.js

--- 7-blogs/src/index.js
+++ 7-blogs/src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+// it will affect all requests sent from anywhere in the app
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    // edit request config
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    // forward the request to the component invoking axios
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
