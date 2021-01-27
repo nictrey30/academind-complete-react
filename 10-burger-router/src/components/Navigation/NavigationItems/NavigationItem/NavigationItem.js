a47301413a46144aa2ddfe5dd32f6e983a01a0d6
Traian <niculescu.traian.ct@gmail.com>
2021-01-27 02:57:18 +0200

    navigation links with Router NavLink in the toolbat

--- 10-burger-router/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ 10-burger-router/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItem.module.css';
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a href={props.link} className={props.active ? classes.active : null}>
+    {/* <a href={props.link} className={props.active ? classes.active : null}>
       {props.children}
-    </a>
+    </a> */}
+    <NavLink
+      to={props.link}
+      activeClassName={classes.active}
+      exact={props.exact}
+    >
+      {props.children}
+    </NavLink>
   </li>
 );
 
